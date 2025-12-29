5cd605dccdcea3b92add4005df16291a9b4d6d32
uthso66 <tarikulislam.uthso.3966@gmail.com>
2025-12-29 21:42:50 +0600

    ✨ Add Navbar and Hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export default function App() {
   return (
-    <div className="flex-center h-screen">
-      <h1 className="text-3xl text-indigo-300">Hello, World!</h1>
-    </div>
+    <main>
+      <Navbar />
+      <Hero />
+    </main>
   );
 }
